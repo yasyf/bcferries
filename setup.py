992468761b73f1e07f2d6c1b0a60e364faff50ba
Yasyf Mohamedali <yasyfm@gmail.com>
2014-12-28 02:30:07 -0800

    Support Python 3

--- setup.py
+++ setup.py
@@ -25,5 +25,18 @@ setup(
     'python-dateutil',
     'functools32',
     'python-Levenshtein'
-  ]
+  ],
+  classifiers= [
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4'
+  ],
+   use_2to3 = True
 )
